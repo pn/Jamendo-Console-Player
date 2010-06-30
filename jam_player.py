ab06b788777d5faaff8bbf9d3f886405d42aed39
Satanowski <satanowski@gmail.com>
2010-06-30 12:41:28 +0200

    Parsing more than just tracks ids. Displaying artist,album and track name.

--- jam_player.py
+++ jam_player.py
@@ -11,9 +11,21 @@ class JamendoFetcher():
 		self._getTracksIds(self.albumId)
 
 	def _getTracksIds(self,albumId):
-		url = 'http://www.jamendo.com/pl/get2/track_id/track/xml/track_album+album_artist/?order=numalbum_asc&n=50&album_id=%d' % (albumId)
-		trackurl = 'http://www.jamendo.com/pl/get2/stream/track/redirect/?id=%d&streamencoding=mp31'
-		self.tracks = [ trackurl % (int(num)) for num in re.findall('<track>(\d+)</track>', urllib.urlopen(url).read())]
+		url = 'http://www.jamendo.com/pl/get2/track_id+artist_name+album_name+name/track/xml/track_album+album_artist/?order=numalbum_asc&n=50&album_id=%d' % (albumId)
+		trackurl = 'http://www.jamendo.com/pl/get2/stream/track/redirect/?id=%s&streamencoding=mp31'
+		keys = ['track_id','artist_name','album_name','name']
+		
+		for line in urllib.urlopen(url).read().split('<track>'):
+			track = {}
+			for key in keys:
+				data = re.findall('<%s>(.*)</%s>' % (key,key), line)
+				if len(data)>0:
+					track[key] = data[0]
+				else:
+					continue
+			if len( track.keys() ) == len( keys ): #all data parsed correctly
+				track['url'] = trackurl % (track['track_id'])
+				self.tracks.append(track)
 
 	def _fetchTrack(self,trackUrl):
 		'''Download the file and return the name of the file '''
@@ -25,7 +37,7 @@ class JamendoFetcher():
 		'''Download all tracks and store them in files of names starting with 'prefix' '''
 		x = 1
 		for track in self.tracks:
-			fname = self._fetchTrack(track)
+			fname = self._fetchTrack(track['url'] )
 			newfile = '%s_%d.mp3' % (prefix,x)
 			sh.move(fname, newfile)
 			x += 1
@@ -43,16 +55,17 @@ if __name__ == '__main__':
 		parser.error("Incorrect number of arguments")
 	try:
 		aId = int(args[0])
-		jf = JamendoFetcher(aId)
-		if options.download:
-			if options.prefix != False:
-				jf.downloadTracks(options.prefix)
-			else:
-				jf.downloadTracks()
-		if options.play:
-			for track in jf.tracks:
-				print 'Playing %s' % track
-				subprocess.call(['mpg123', track],stdout=open('/dev/null', 'w'), stderr=subprocess.STDOUT)
-				print
 	except:
 		parser.error("Wrong AlbumId")
+		sys.exit()
+	jf = JamendoFetcher(aId)
+	if options.download:
+		if options.prefix != False:
+			jf.downloadTracks(options.prefix)
+		else:
+			jf.downloadTracks()
+	if options.play:
+		for track in jf.tracks:
+			print 'Playing:\n\tArtist: %s\n\tAlbum: %s\n\tTitle:%s' %  ( track['artist_name'] ,track['album_name'] ,track['name'] )
+			subprocess.call(['mpg123', track['url'] ],stdout=open('/dev/null', 'w'), stderr=subprocess.STDOUT)
+			print
